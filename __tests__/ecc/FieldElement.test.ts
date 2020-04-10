770e4e0c24b6ab8bc6a40ad5fd2c4d2154060551
Brian Mancini <bmancini@gmail.com>
2020-04-10 14:12:58 -0400

    ecc: implements FieldElement
    Implements in accordance with Chapter 1 on finite fields

---
+++ __tests__/ecc/FieldElement.test.ts
@@ -0,0 +1,138 @@
+import { expect } from "chai";
+import { FieldElement } from "../../src/ecc/FieldElement";
+
+describe("FieldElement", () => {
+  describe(".toString()", () => {
+    it("should stringify correctly", () => {
+      expect(new FieldElement(7n, 13n).toString()).to.equal(
+        "FieldElement_13(7)"
+      );
+    });
+  });
+
+  describe(".equals()", () => {
+    it("should return false for undefined", () => {
+      const a = new FieldElement(7n, 13n);
+      const b = undefined;
+      expect(a.equals(b)).to.equal(false);
+    });
+
+    it("should return true for equal", () => {
+      const a = new FieldElement(7n, 13n);
+      const b = new FieldElement(7n, 13n);
+      expect(a.equals(b)).to.equal(true);
+    });
+
+    it("should return false for different num", () => {
+      const a = new FieldElement(7n, 13n);
+      const b = new FieldElement(8n, 13n);
+      expect(a.equals(b)).to.equal(false);
+    });
+
+    it("should return false for different prime", () => {
+      const a = new FieldElement(7n, 13n);
+      const b = new FieldElement(7n, 17n);
+      expect(a.equals(b)).to.equal(false);
+    });
+  });
+
+  describe(".add()", () => {
+    it("should throw when not in same field", () => {
+      const a = new FieldElement(7n, 13n);
+      const b = new FieldElement(1n, 17n);
+      expect(() => a.add(b)).to.throw();
+    });
+
+    it("should not wrap", () => {
+      const a = new FieldElement(7n, 13n);
+      const b = new FieldElement(1n, 13n);
+      expect(a.add(b)).to.deep.equal(new FieldElement(8n, 13n));
+    });
+
+    it("should wrap", () => {
+      const a = new FieldElement(7n, 13n);
+      const b = new FieldElement(6n, 13n);
+      expect(a.add(b)).to.deep.equal(new FieldElement(0n, 13n));
+    });
+  });
+
+  describe(".sub()", () => {
+    it("should throw when not in same field", () => {
+      const a = new FieldElement(7n, 13n);
+      const b = new FieldElement(1n, 17n);
+      expect(() => a.sub(b)).to.throw();
+    });
+
+    it("should not wrap", () => {
+      const a = new FieldElement(7n, 13n);
+      const b = new FieldElement(1n, 13n);
+      expect(a.sub(b)).to.deep.equal(new FieldElement(6n, 13n));
+    });
+
+    it("should wrap", () => {
+      const a = new FieldElement(7n, 13n);
+      const b = new FieldElement(8n, 13n);
+      expect(a.sub(b)).to.deep.equal(new FieldElement(12n, 13n));
+    });
+  });
+
+  describe(".mul()", () => {
+    it("should throw when not in same field", () => {
+      const a = new FieldElement(7n, 13n);
+      const b = new FieldElement(1n, 17n);
+      expect(() => a.mul(b)).to.throw();
+    });
+
+    it("should not wrap", () => {
+      const a = new FieldElement(3n, 13n);
+      const b = new FieldElement(2n, 13n);
+      expect(a.mul(b)).to.deep.equal(new FieldElement(6n, 13n));
+    });
+
+    it("should wrap", () => {
+      const a = new FieldElement(7n, 13n);
+      const b = new FieldElement(3n, 13n);
+      expect(a.mul(b)).to.deep.equal(new FieldElement(8n, 13n));
+    });
+  });
+
+  describe(".div()", () => {
+    it("should throw when not in same field", () => {
+      const a = new FieldElement(7n, 13n);
+      const b = new FieldElement(1n, 17n);
+      expect(() => a.div(b)).to.throw();
+    });
+
+    it("should not wrap", () => {
+      const a = new FieldElement(6n, 13n);
+      const b = new FieldElement(2n, 13n);
+      expect(a.div(b)).to.deep.equal(new FieldElement(3n, 13n));
+    });
+
+    it("should wrap", () => {
+      const a = new FieldElement(2n, 19n);
+      const b = new FieldElement(7n, 19n);
+      expect(a.div(b)).to.deep.equal(new FieldElement(3n, 19n));
+    });
+  });
+
+  describe(".pow()", () => {
+    it("should not wrap", () => {
+      const a = new FieldElement(3n, 13n);
+      const e = 2n;
+      expect(a.pow(e)).to.deep.equal(new FieldElement(9n, 13n));
+    });
+
+    it("should wrap", () => {
+      const a = new FieldElement(3n, 13n);
+      const e = 3n;
+      expect(a.pow(e)).to.deep.equal(new FieldElement(1n, 13n));
+    });
+
+    it("should handle negative exponent", () => {
+      const a = new FieldElement(7n, 13n);
+      const e = -3n;
+      expect(a.pow(e)).to.deep.equal(new FieldElement(8n, 13n));
+    });
+  });
+});
---
+++ src/ecc/FieldElement.ts
@@ -0,0 +1,99 @@
+/**
+ * Represents a finite field element which represents an elmement in the field
+ * F_prime.
+ */
+export class FieldElement {
+  public num: bigint;
+  public prime: bigint;
+
+  constructor(num: bigint, prime: bigint) {
+    if (num >= prime || num < 0n) {
+      throw new Error(`Num ${num} not in field range 0 ${prime - 1n}`);
+    }
+    this.num = num;
+    this.prime = prime;
+  }
+
+  public toString() {
+    return `FieldElement_${this.prime}(${this.num})`;
+  }
+
+  public equals(other: FieldElement) {
+    if (!other) return false;
+    return this.prime === other.prime && this.num === other.num;
+  }
+
+  /**
+   * Adds two numbers in the same Field by using the
+   * formula: `(a + b) % p`
+   */
+  public add(other: FieldElement): FieldElement {
+    if (this.prime !== other.prime) {
+      throw new Error(`Cannot addd two numbers in different Fields`);
+    }
+    const num = (this.num + other.num) % this.prime;
+    return new FieldElement(num, this.prime);
+  }
+
+  /**
+   *
+   * @remarks
+   * This fixes the negative mod bug using the formula:
+   *
+   * ```
+   * (n + p) % p
+   * ```
+   * @param other
+   */
+  public sub(other: FieldElement): FieldElement {
+    if (this.prime !== other.prime) {
+      throw new Error(`Cannot add two numbers in different Fields`);
+    }
+    let num = (this.num - other.num) % this.prime;
+    num = (num + this.prime) % this.prime; // fixes negative mod bug
+    return new FieldElement(num, this.prime);
+  }
+
+  public mul(other: FieldElement): FieldElement {
+    if (this.prime !== other.prime) {
+      throw new Error(`Cannot multiply two numbers in different Fields`);
+    }
+
+    const num = (this.num * other.num) % this.prime;
+    return new FieldElement(num, this.prime);
+  }
+
+  /**
+   * Divides one number by another using Fermat's Little Theory which states that
+   * 1 = n^(p-1) % p and means we can find the inverse of using the formula
+   *
+   * ```
+   * (a * b ** (p - 2)) % p
+   * ```
+   * @param other
+   */
+  public div(other: FieldElement): FieldElement {
+    if (this.prime !== other.prime) {
+      throw new Error(`Cannot divide two numbers in different Fields`);
+    }
+    const num = (this.num * other.num ** (this.prime - 2n)) % this.prime;
+    return new FieldElement(num, this.prime);
+  }
+
+  /**
+   * Returns a new FieldElement with the value being the current number
+   * raised to the provided exponent. We first force the exponent to be positive
+   * using Fermats Little Theorem. This uses the formula:
+   *
+   * ```
+   * b = b % (p - 1)
+   * (a ** b) % p
+   * ```
+   * @param exponent
+   */
+  public pow(exponent: bigint): FieldElement {
+    exponent = (exponent + this.prime - 1n) % (this.prime - 1n); // fixes negative mod bug
+    const num = this.num ** exponent % this.prime;
+    return new FieldElement(num, this.prime);
+  }
+}
