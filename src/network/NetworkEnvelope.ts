ede0265f9776abd1df627a424baf8d2b54979f7f
Brian Mancini <bmancini@gmail.com>
2020-04-26 14:55:17 -0400

    networking: adding serialization for network envelope

--- src/network/NetworkEnvelope.ts
+++ src/network/NetworkEnvelope.ts
@@ -1,7 +1,7 @@
 import { StreamReader } from "../util/StreamReader";
 import { Readable } from "stream";
 import { hash256 } from "../util/Hash256";
-import { rstrip } from "../util/BufferUtil";
+import { rstrip, combine } from "../util/BufferUtil";
 
 // tslint:disable-next-line: variable-name
 export const NetworkMagic = Buffer.from("f9beb4d9", "hex");
@@ -80,5 +80,43 @@ export class NetworkEnvelope {
    */
   public toString() {
     return `${this.command}: ${this.payload.toString("hex")}`;
+  }
+
+  /**
+   * Serializes the network message with the following format:
+   *
+   * network magic
+   * command - 12 bytes
+   * payload len - 4 bytes LE
+   * payload checksum - 4 bytes
+   * payload
+   */
+  public serialize(): Buffer {
+    const envelope = Buffer.alloc(4 + 12 + 4 + 4);
+    let offset = 0;
+
+    // network magic, 4 bytes
+    if (this.testnet) {
+      TestnetNetworkMagic.copy(envelope);
+    } else {
+      NetworkMagic.copy(envelope);
+    }
+    offset += 4;
+
+    // command
+    envelope.write(this.command, offset);
+    offset += 12;
+
+    // payload len
+    envelope.writeUInt32LE(this.payload.length, offset);
+    offset += 4;
+
+    // payload checksum
+    const checksum = hash256(this.payload).slice(0, 4);
+    checksum.copy(envelope, offset);
+    offset += 4;
+
+    // return envelop + payload
+    return combine(envelope, this.payload);
   }
 }
